8ee2528c8b15b479922223d557ae08d43cc01866
Julia <uliacop@gmail.com>
2026-08-03 21:03:03 +0300

    change acsses  to picture

--- src/components/Logo/Logo.jsx
+++ src/components/Logo/Logo.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function Logo() {
   return (
     <h1>
-      <img src="/src/img/signs/mainicons.png" />
+      <img src="/img/signs/mainicons.png" />
     </h1>
   );
 }
